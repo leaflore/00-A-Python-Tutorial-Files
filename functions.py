1461da82b0879b925250c934395db6da1221084e
leaflore <br.kemp1@gmail.com>
2025-12-01 02:28:54 -0800

    20251201 - Course Progress Functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+#Functions
+
+#Function containing a variable
+def hello(name):
+    print('Hello! ' + name)
+
+hello('Brandon')
+
+#Function containing two variables and one variable is converted to a
+#string
+def hello2(name, age):
+    print('Hello ' + name + ' you are ' + str(age) + ' years old' )
+
+hello2('Brandon', 37)
