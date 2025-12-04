379910fcd1675511dc11a200eb62725e70ae3e79
leaflore <br.kemp1@gmail.com>
2025-12-03 18:39:47 -0800

    20251203 - Course Progress

--- functions.py
+++ functions.py
@@ -12,3 +12,25 @@ def hello2(name, age):
     print('Hello ' + name + ' you are ' + str(age) + ' years old' )
 
 hello2('Brandon', 37)
+
+def talk(phrase):
+    def say(word):
+        print(word)
+        
+    words = phrase.split(' ')
+    for word in words:
+        say(word)
+        
+talk('I am going to buy the milk')
+
+def count():
+    count = 0
+    
+    def increment():
+        nonlocal count
+        count = count + 1
+        print(count)
+        
+    increment()
+    
+count()
